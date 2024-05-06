faba69c391025b721609348a2a5593973869d35e
ReHyun <98667966+ReHyun@users.noreply.github.com>
2024-05-06 01:19:35 +0000

    React : useContext

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,25 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { UserDispatch } from './App';
 
-function User({ user, onRemove, onToggle }) {
-  useEffect(() => {
-    //console.log(user);
-  });
+const User = React.memo(function User({ user }) {
+  const dispatch = useContext(UserDispatch);
+
   return (
     <div>
-      <b style={{ cursor: 'pointer', color: user.active ? 'green' : 'black' }} onClick={() => onToggle(user.id)}>
+      <b
+        style={{ cursor: 'pointer', color: user.active ? 'green' : 'black' }}
+        onClick={() => {
+          dispatch({ type: 'TOGGLE_USER', id: user.id });
+        }}
+      >
         {user.username}
       </b>
       <span>({user.email})</span>
-      <button onClick={() => onRemove(user.id)}>삭제</button>
+      <button onClick={() => {
+        dispatch({type:"REMOVE_USER", id:user.id})
+      }}>삭제</button>
     </div>
   );
-}
+});
 
-function UserList({ users, onRemove, onToggle }) {
+function UserList({ users }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
+        <User user={user} key={user.id} />
       ))}
     </div>
   );
