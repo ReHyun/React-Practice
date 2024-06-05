e3ac52f3da53ed2decb6229dc97674468ca237a4
ReHyun <98667966+ReHyun@users.noreply.github.com>
2024-06-05 05:00:16 +0000

    TodoList : TodoList Component 완성

--- mashup-todolist/src/TodoContext.js
+++ mashup-todolist/src/TodoContext.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, createContext, useContext, useRef } from 'react';
 
 const initialTodos = [
   {
@@ -30,13 +30,49 @@ function todoReducer(state, action) {
     case 'TOGGLE':
       return state.map((todo) => (todo.id === action.id ? { ...todo, done: !todo.done } : todo));
     case 'REMOVE':
-      return;
+      return state.filter((todo) => todo.id !== action.id);
     default:
-      throw new Error(`Unhandled action type : ${action.type}`);
+      throw new Error(`Unhandled action type: ${action.type}`);
   }
 }
 
+const TodoStateContext = createContext();
+const TodoDispatchContext = createContext();
+const TodoNextIdContext = createContext();
+
 export function TodoProvider({ children }) {
   const [state, dispatch] = useReducer(todoReducer, initialTodos);
-  return children;
+  const nextId = useRef(5);
+
+  return (
+    <TodoStateContext.Provider value={state}>
+      <TodoDispatchContext.Provider value={dispatch}>
+        <TodoNextIdContext.Provider value={nextId}>{children}</TodoNextIdContext.Provider>
+      </TodoDispatchContext.Provider>
+    </TodoStateContext.Provider>
+  );
+}
+
+export function useTodoState() {
+  const context = useContext(TodoStateContext);
+  if (!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
+}
+
+export function useTodoDispatch() {
+  const context = useContext(TodoDispatchContext);
+  if (!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
+}
+
+export function useTodoNextId() {
+  const context = useContext(TodoNextIdContext);
+  if (!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
 }
