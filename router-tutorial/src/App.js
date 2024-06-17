5e9a1e6a48404aba96b785db0d7dda5a455ba7ac
ReHyun <98667966+ReHyun@users.noreply.github.com>
2024-06-17 03:29:16 +0000

    React Router : useNavigate()

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -4,13 +4,16 @@ import Article from './pages/Article';
 import Articles from './pages/Articles';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Layout from './Layout';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
